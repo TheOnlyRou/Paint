b8832f5bb6838dc59badf8a75679abb15ab4d2b5
MaRouane <34210209+TheOnlyRou@users.noreply.github.com>
2018-04-15 16:58:21 +0200

    2 new interfaces
    Added 2 interfaces

--- src/paint/controller/DrawingEngine.java
+++ src/paint/controller/DrawingEngine.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package paint.view;
+package paint.controller;
 
 /**
  *
  * @author user
  */
-public class Paint {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public interface DrawingEngine {
     
 }
